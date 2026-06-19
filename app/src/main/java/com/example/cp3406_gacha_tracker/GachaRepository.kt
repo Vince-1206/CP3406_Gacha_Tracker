ad53b5914412ce9160fc207f7742d197dc279585
Vince <vincelin126@gmail.com>
2026-06-19 19:17:11 +0800

    Add the image URL

--- app/src/main/java/com/example/cp3406_gacha_tracker/GachaRepository.kt
+++ app/src/main/java/com/example/cp3406_gacha_tracker/GachaRepository.kt
@@ -10,12 +10,12 @@ class GachaRepository {
 
     fun getFallbackCharacters(): List<GachaCharacter> {
         return listOf(
-            GachaCharacter("Astra", 5, "Fire", "https://via.placeholder.com/150"),
-            GachaCharacter("Nova", 5, "Lightning", "https://via.placeholder.com/150"),
-            GachaCharacter("Luna", 4, "Ice", "https://via.placeholder.com/150"),
-            GachaCharacter("Maple", 4, "Water", "https://via.placeholder.com/150"),
-            GachaCharacter("Milo", 3, "Wind", "https://via.placeholder.com/150"),
-            GachaCharacter("Kai", 3, "Earth", "https://via.placeholder.com/150")
+            GachaCharacter("Astra", 5, "Fire", "https://www.image2url.com/r2/default/images/1781866238083-ea1f30c3-630d-4db2-97d7-f2fec2fd03a2.jpg"),
+            GachaCharacter("Nova", 5, "Lightning", "https://www.image2url.com/r2/default/images/1781866465910-f6646192-b319-4598-9fa2-9403049b6138.jpg"),
+            GachaCharacter("Luna", 4, "Ice", "https://www.image2url.com/r2/default/images/1781866505366-44e74fb1-1241-4970-a8d6-9c36670118ca.jpg"),
+            GachaCharacter("Maple", 4, "Water", "https://www.image2url.com/r2/default/images/1781866593542-65403ddf-ee3a-4db8-b410-850353f5033b.jpg"),
+            GachaCharacter("Milo", 3, "Wind", "https://www.image2url.com/r2/default/images/1781866905032-e348d1e2-46c8-46b4-868b-bb801d7ae2b9.jpg"),
+            GachaCharacter("Kai", 3, "Earth", "https://www.image2url.com/r2/default/images/1781866814848-cfd6622f-68e2-44a4-b3bb-2615c24bbb83.jpg")
         )
     }
 }
